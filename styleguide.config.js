be76a5f6e0c533a25b8145fbf1cb65455e555f64
米老朱 <laozhu.me@gmail.com>
2018-12-26 16:29:54 +0800

    Move babel config to babel.config.js

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,44 +2,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // eslint-disab
 
 const isDev = process.env.NODE_ENV === 'development';
 
-// Respect user's environment
-const presetEnv = [
-  '@babel/preset-env',
-  {
-    loose: true,
-    debug: false,
-    modules: false,
-    useBuiltIns: 'usage',
-    targets: {
-      node: 'current',
-      esmodules: false,
-    },
-  },
-];
-
-// JSX enabled
-const presetReact = [
-  '@babel/preset-react',
-  {
-    development: isDev,
-  },
-];
-
-// Living on the edge
-const plugins = [
-  ['@babel/plugin-syntax-dynamic-import'],
-  ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
-  ['@babel/plugin-proposal-class-properties'],
-  ['@babel/plugin-proposal-export-default-from'],
-  ['@babel/plugin-proposal-export-namespace-from'],
-  ['@babel/plugin-proposal-logical-assignment-operators'],
-  ['@babel/plugin-proposal-nullish-coalescing-operator'],
-  ['@babel/plugin-proposal-numeric-separator'],
-  ['@babel/plugin-proposal-optional-chaining'],
-  ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-  ['@babel/plugin-proposal-throw-expressions'],
-];
-
 module.exports = {
   title: 'Pubean Components',
   version: '0.1.0',
@@ -57,10 +19,6 @@ module.exports = {
     favicon: 'favicon.ico',
   },
   theme: {
-    color: {
-      link: '#00ab6b',
-      linkHover: '#00ab6b',
-    },
     fontFamily: {
       base: [
         'Chinese Quote',
@@ -87,13 +45,7 @@ module.exports = {
         {
           test: /\.js?$/,
           exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [presetEnv, presetReact],
-              plugins,
-            },
-          },
+          use: { loader: 'babel-loader' },
         },
         {
           test: /\.css$/,
